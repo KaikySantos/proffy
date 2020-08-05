032536ee4304f38c1ee3493ce48db839c5c78db5
KaikySantos <info.kaiky@gmail.com>
2020-08-05 16:18:13 -0300

    Página de listagem + filtragem de proffys

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,32 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+	id: number;
+	avatar: string;
+	bio: string;
+	cost: number;
+	name: string;
+	subject: string;
+	whatsapp: string;
+};
+
+interface TeacherItemProps {
+	teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection(){
+		api.post('/connections', {
+			user_id: teacher.id,
+		})
+	}
+
     return(
         <article className="teacher-item">
 
 					<header>
-						<img src="https://avatars3.githubusercontent.com/u/56506919?s=460&u=6b162e9fec99aa6e4991bc62feb0cf9f88d3e3e5&v=4" alt="Kaiky Santos" />
+						<img src={teacher.avatar} alt="Kaiky Santos" />
 						<div>
-							<strong>Kaiky Santos</strong>
-							<span>Hacker</span>
+							<strong>{teacher.name}</strong>
+							<span>{teacher.subject}</span>
 						</div>
 					</header>
 
 					<p>
-						Quer aprender Hacker empresas como a Nasa, Google ou Facebook ? Então cuida!
+						{teacher.bio}
 					</p>
 
 					<footer>
 						<p>
 							Preço/Hora
-							<strong>R$ 29.889,90</strong>
+							<strong>R$ {teacher.cost}</strong>
 						</p>
-						<button type="button">
+						<a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
 							<img src={whatsappIcon} alt="Whatsapp"/>
 							Entrar em contato
-						</button>
+						</a>
 					</footer>
 
 				</article>
